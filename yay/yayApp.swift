2e7a9340b786f5a71e755f6d0fb2960c63df6977
kmhy1015 <tophye@gmail.com>
2026-04-27 04:17:42 -0700

    Initial Commit

---
+++ yay/yayApp.swift
@@ -0,0 +1,17 @@
+//
+//  yayApp.swift
+//  yay
+//
+//  Created by KitahashiM on 2026/4/27.
+//
+
+import SwiftUI
+
+@main
+struct yayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
